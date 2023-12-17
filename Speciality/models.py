6d7d714c9cfe721e63a864d0c9a34c2815a8cd16
Alireza <alirezagoldoos@gmail.com>
2023-12-17 22:36:56 +0330

    new edits

--- Speciality/models.py
+++ Speciality/models.py
@@ -2,41 +2,24 @@ from django.db import models
 from django.utils import timezone
 
 class speciality(models.Model):
-    Name_Specialty = models.CharField(max_length=100)
-
-
+    Name = models.CharField(max_length=100)
 
 class doctor(models.Model):
     Name=models.CharField(max_length=200)
-    Family=models.CharField(max_length=200)
-    Specialty = models.ForeignKey('speciality', on_delete=models.CASCADE, null=True)
-    Gender=models.BooleanField(help_text=" Light tick for male , Hide tick for female") # True for male , False for female
-    Scores=models.FloatField(help_text="Please be between 1 and 5")
-    PhoneNumber=models.CharField(max_length=13)
-    Address=models.TextField()
-
+    Specialty = models.ForeignKey(speciality, on_delete=models.CASCADE)
+    Sum_Scores=models.FloatField(default=0, null=True)
+    Count_Scores=models.FloatField(default=0, null=True)
 
 class patient(models.Model):
     Name=models.CharField(max_length=200)
-    Family=models.CharField(max_length=200)
     Age=models.PositiveIntegerField()
-    PhoneNumber=models.CharField(max_length=13)
-    
-    
-
-
 
 class reserve(models.Model):
-    Name=models.CharField(max_length=200)
-    Family=models.CharField(max_length=200)
-    PhoneNumber=models.CharField(max_length=13)
-    DateAndTime=models.DateTimeField(default=timezone.now)
-    Name_Doctor=models.ForeignKey("doctor",on_delete=models.PROTECT)
- 
-
-    
-
+    bimar = models.ForeignKey(patient, on_delete=models.CASCADE)
+    docti = models.ForeignKey(doctor, on_delete=models.CASCADE)
+    time = models.DateTimeField()
 
 class comment(models.Model):
-    Text=models.TextField()
-    PatientName=models.ForeignKey("patient",on_delete=models.PROTECT)
+    user = models.CharField()
+    comment = models.TextField()
+    doctor = models.ForeignKey(doctor, on_delete=models.CASCADE)
