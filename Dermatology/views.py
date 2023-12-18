36a595ae2f2e61e6f1a9b7c9386c75da8d30806a
Alireza <alirezagoldoos@gmail.com>
2023-12-18 14:29:31 +0330

    new commit

--- Dermatology/views.py
+++ Dermatology/views.py
@@ -1,11 +1,19 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from Speciality.models import doctor
 
 # Create your views here.
-def List():
+def List(request):
     doctor_list = doctor.objects.all()
+    derma_doctor_list = []
     for doci in doctor_list:
-        if doci.Specialty
+        if doci.Specialty.Name == 'Dermatology':
+            derma_doctor_list.append({
+                'Name' : doci.Name,
+                'Speciality' : doci.Specialty.Name
+            })
+    return JsonResponse(request, derma_doctor_list, safe=False)
+
 
 
 
--- Speciality/models.py
+++ Speciality/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+# from django.utils import timezone
 
 class speciality(models.Model):
     Name = models.CharField(max_length=100)
