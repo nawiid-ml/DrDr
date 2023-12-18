33697378e442093196a28c4ccfce609c8647b652
Alireza <alirezagoldoos@gmail.com>
2023-12-18 12:51:30 +0330

    new commit

--- Speciality/admin.py
+++ Speciality/admin.py
@@ -5,25 +5,17 @@ from .models import speciality, doctor, patient, reserve, comment
 class speciality_admin(ModelAdmin):
     pass
 
-
-
 @register(doctor)
 class doctor_admin(ModelAdmin):
     pass
-
-
 
 @register(patient)
 class patient_admin(ModelAdmin):
     pass
 
-
-
 @register(reserve)
 class reserve_admin(ModelAdmin):
     pass
-
-
 
 @register(comment)
 class comment_admin(ModelAdmin):
