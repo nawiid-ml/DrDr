991748bd998044370c07429a570f2b8b5807f5b5
nawiid-ml <navidmala495@gmail.com>
2023-12-19 23:43:31 +0330

    update urls 2

--- Specialty/urls.py
+++ Specialty/urls.py
@@ -4,4 +4,11 @@ from .views import List
 urlpatterns = [
     path('', List),
     path('dermatology/', include('Dermatology.urls')),
+    path('Pulmonology/',include('Pulmonology.urls')),
+    path('Neurology/',include('Neurology.urls')),
+    path('Nephrology/',include('Nephrology.urls')),
+    path('Hematology/',include('Hematology.urls')),
+    path('Dermatology/',include('Dermatology.urls')),
+    path('Cardiology/',include('Cardiology.urls'))
 ]
+
