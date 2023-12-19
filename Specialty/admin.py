e1c067d7c955c0537ca416b4812d61cf75032e0a
Alireza <alirezagoldoos@gmail.com>
2023-12-19 17:38:54 +0330

    fixing pannel admin

--- Specialty/admin.py
+++ Specialty/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin
-
+from django.contrib.admin import ModelAdmin, register
+from .models import speciality, doctor, patient, reserve, comment
 # Register your models here.
+
+@register(speciality)
+class speciality_admin(ModelAdmin):
+    pass
+
+@register(doctor)
+class doctor_admin(ModelAdmin):
+    pass
+
+@register(patient)
+class patient_admin(ModelAdmin):
+    pass
+
+@register(reserve)
+class reserve_admin(ModelAdmin):
+    pass
+
+@register(comment)
+class comment_admin(ModelAdmin):
+    pass
+
+
