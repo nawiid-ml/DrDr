72eb8f01de45cfe6ebbcf597dda17f004749637b
Alireza <alirezagoldoos@gmail.com>
2023-12-19 17:12:43 +0330

    new

--- DrDr/urls.py
+++ DrDr/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Speciality/',include('Speciality.urls'))
+    # path('Speciality',include('Speciality.urls'))
 ]
--- Speciality/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-#
-
-urlpatterns=[
-    #path("list_test",List_test,name='list_test')
-]
